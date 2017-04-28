afc12c62a4ec5e03a095d9e927f20fb273ec26f9
machao <machao14@baidu.com>
2017-04-28 10:57:31 +0800

    other2

---
+++ app/src/main/java/com/cfox/openshareutils/other2/other2.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+package com.cfox.openshareutils.other2;
+
+/**
+ * Created by machao14 on 2017/4/28.
+ */
+
+public class other2 {
+}
